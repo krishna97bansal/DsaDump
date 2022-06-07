d0be62e8d665499f218c21c35db742bf25b44cc6
krishna97bansal <krishna97bansal@gmail.com>
2022-06-07 17:24:45 +0530

    Project commit

---
+++ src/main/java/com/dsa/DSADump/DsaDumpApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.DSADump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaDumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaDumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsa/DSADump/DsaDumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.DSADump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaDumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
