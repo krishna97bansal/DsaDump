79e0280b4403248c25e0c7e4f79fc61240afc958
krishna97bansal <krishna97bansal@gmail.com>
2022-06-16 19:08:05 +0530

    Updated Ones

---
+++ src/main/java/com/dsa/DSADump/graph/DirectedCycleDetectionViaDFS.java
@@ -0,0 +1,73 @@
+package com.dsa.DSADump.graph;
+
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class DirectedCycleDetectionViaDFS {
+
+    public boolean driver()
+    {
+        int vertex=9;
+        List<List<Integer>> adjacencyList=new LinkedList<>();
+        for(int i=0;i<=vertex;i++){
+            adjacencyList.add(new LinkedList<>());
+        }
+
+        adjacencyList.get(1).add(2);
+        adjacencyList.get(2).add(3);
+
+        adjacencyList.get(3).add(4);
+        adjacencyList.get(3).add(6);
+
+        adjacencyList.get(4).add(5);
+
+        adjacencyList.get(6).add(5);
+
+        adjacencyList.get(7).add(2);
+
+        adjacencyList.get(7).add(8);
+
+        adjacencyList.get(8).add(9);
+
+        adjacencyList.get(9).add(7);
+
+     return check(vertex,adjacencyList);
+
+    }
+
+    private boolean check(int vertex, List<List<Integer>> adjacencyList) {
+
+        int visited[]=new int[vertex+1];
+        int dfsVisited[]=new int[vertex+1];
+
+        for(int i=1;i<=vertex;i++)
+        {
+            if(visited[i]==0){
+                if(findCycle(i,dfsVisited,visited,adjacencyList)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean findCycle(int i, int[] dfsVisited, int[] visited, List<List<Integer>> adjacencyList) {
+        visited[i]=1;
+        dfsVisited[i]=1;
+        for(int neighbours: adjacencyList.get(i)){
+            if(visited[neighbours]==0) {
+                if (findCycle(neighbours, dfsVisited, visited, adjacencyList)) {
+                    return true;
+                }
+            }
+                else if (dfsVisited[neighbours]==1) {
+                    return true;
+                }
+        }
+        dfsVisited[i]=0;
+        return false;
+    }
+}
